7c76476b2ced0a81ea7db159dea790576940d74d
Yancey Thomas <ythomas@leaders6-12.org>
2015-08-10 05:32:52 +0000

    finally working

--- application_controller.rb
+++ application_controller.rb
@@ -1,3 +1,27 @@
 require 'bundler'
 Bundler.require
-class MyApp < Sinatra::Base
+class MyApp < Sinatra::Base 
+  
+  get '/' do
+    erb :index
+  end
+
+get '/stay_home.html' do
+  erb :stay_home
+end
+get '/escape_into_lab.html' do
+  erb :escape_into_lab
+end
+get '/get_your_brother.html' do
+  erb :get_your_brother
+end
+get '/leave_the_school_.html' do
+  erb :leave_the_school_
+end
+get '/cure_for_mankind.html' do
+  erb :cure_for_mankind
+end
+get '/go_to_the_hospital.html' do
+  erb :go_to_the_hospital
+end
+end
